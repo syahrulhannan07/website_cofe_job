6052bdf80530a3618fe7dc485edc7ed7c196cade
syahrulhannan07 <syahrulhannan07@gmail.com>
2026-04-14 15:10:07 +0700

    updt a

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('landing'); // Buat file ini jika perlu
-})->name('landing');
-
 Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
