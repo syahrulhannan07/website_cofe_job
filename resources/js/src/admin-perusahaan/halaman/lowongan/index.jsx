092558df8c074587954ce49a7f721a48adcec289
ramadhansanjaya18 <ramadhansanjaya18@gmail.com>
2026-04-30 20:23:22 +0700

    comit baru1

---
+++ resources/js/src/admin-perusahaan/halaman/lowongan/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const HalamanLowongan = () => {
+    return (
+        <div className="flex-1 w-full flex flex-col p-8 lg:p-10 bg-[#F3EDE6] min-h-screen">
+<h1 className="font-poppins font-bold text-3xl text-[#4B2E2B]">Halaman Lowongan</h1>
+        <p className="text-[#7A6153] mt-4">Konten halaman lowongan sedang dalam pengembangan.</p>
+        </div>
+    );
+};
+
+export default HalamanLowongan;
